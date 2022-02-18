58a55b21255c18aec5ee7055a5122aa544c3e115
Viktor Carlsson <99viktor.vc@gmail.com>
2022-02-18 11:18:44 +0100

    Added parsed region data

--- DataParse/parse.py
+++ DataParse/parse.py
@@ -1,4 +1,3 @@
-from xml.etree.ElementTree import tostring
 import pandas as pd
 
 arrivals = pd.read_excel("unwto-all-data-download.xlsx", sheet_name = 1, skiprows = lambda x: x < 2 or (x > 2 and (x-3) % 6 in [1, 3, 4, 5]) or x > 1340, usecols="D, F, L:AJ")
@@ -25,8 +24,16 @@ arrivals["Total"] = arrivals[column_list].sum(axis = 1, numeric_only=float) # Cr
 
 for column in arrivals:
     if column in column_list:
-        arrivals.rename(columns={column: "y" + str(column)}, inplace=True)
+        arrivals.rename(columns={column: "y" + str(column)}, inplace=True) # Add "y" in front of all years for ease of implementation in JS
 
 arrivals.to_csv("Arrivals.csv") # Save the modified data.
 
-regions = pd.read_excel("unwto-all-data-download.xlsx", sheet_name = 2)
+regions = pd.read_excel("unwto-all-data-download.xlsx", sheet_name = 2, skiprows = lambda x: x < 2 or (x > 2 and (x-3) % 11 in [1, 2, 10, 11]) or x > 2456, usecols="D, G, L:AJ")
+regions.rename(columns={"Basic data and indicators": "Country", "Unnamed: 6": "Region of Origin"}, inplace=True) # Make the even rows into numerically sequential indexes
+regions["Country"].fillna(method="ffill", inplace=True)
+regions.dropna(thresh = 2, inplace=True)
+for column in regions:
+    if column in column_list:
+        regions.rename(columns={column: "y" + str(column)}, inplace=True) # Add "y" in front of all years for ease of implementation in JS
+print(regions.head(10))
+regions.to_csv("Regions.csv") # Save the modified data.
