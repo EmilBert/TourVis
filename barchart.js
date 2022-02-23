178e5d6688859b36ea80f5aadb7db61706ff818e
Alice <alicedromberg@gmail.com>
2022-02-23 15:04:12 +0100

    nonworking barchart
    ja asså som ni ser i beskrivningen, en barchart som inte funkar, den kommer inte ens upp om man gör något i applikationen. Good fucking luck att göra något med den här högen med skit.

---
+++ barchart.js
@@ -0,0 +1,14 @@
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svgA = d3.select("#d3-container")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
